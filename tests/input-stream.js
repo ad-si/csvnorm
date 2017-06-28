1037297d74bc456698ab9af20e8d30b771e98c49
Adrian Sieber <mail@adriansieber.com>
2017-06-28 14:28:32 +0000

    Test normalization from stdin

--- tests/input-stream.js
+++ tests/input-stream.js
@@ -1,17 +1,16 @@
 const fs = require('fs')
 const path = require('path')
 const assert = require('assert')
-const {execFile} = require('child_process')
+const {exec} = require('child_process')
 const expectedOutput = fs.readFileSync(
   path.join(__dirname, 'banking/expected-output.csv'),
   'utf-8'
 )
 
-process.stdout.write('Format banking CSV file')
+process.stdout.write('Format banking CSV file from stdin')
 
-execFile(
-  './cli.js',
-  ['tests/banking/input-latin1.csv'],
+exec(
+  'cat tests/banking/input-latin1.csv | ./cli.js',
   {cwd: path.join(__dirname, '../')},
   (error, stdout, stderr) => {
     if (stderr) console.error(stderr)
