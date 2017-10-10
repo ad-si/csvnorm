f6b22e3065a233d91ea52989238affaef9bb8168
Adrian Sieber <mail@adriansieber.com>
2017-10-10 16:20:43 +0000

    Fix incorrect date regex

--- source/formatDate.js
+++ source/formatDate.js
@@ -1,18 +1,18 @@
 module.exports = value => {
   if (typeof value !== 'string') return null
 
-  const mmddyyyy = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/
+  const mmddyyyy = /^([01][0-9])\/([0-3][0-9])\/([0-9]{4})$/
   if (mmddyyyy.test(value)) {
     console.error()
     return value.replace(mmddyyyy, '$3-$1-$2')
   }
 
-  const ddmmyyyy = /^([0-9]{2})\.([0-9]{2})\.([0-9]{4})$/
+  const ddmmyyyy = /^([0-3][0-9])\.([01][0-9])\.([0-9]{4})$/
   if (ddmmyyyy.test(value)) {
     return value.replace(ddmmyyyy, '$3-$2-$1')
   }
 
-  const ddmmyy = /^([0-3][0-9])\.([01][1-9])\.([0-9]{2})$/
+  const ddmmyy = /^([0-3][0-9])\.([01][0-9])\.([0-9]{2})$/
   if (ddmmyy.test(value)) {
     return value.replace(ddmmyy, '20$3-$2-$1')
   }
